278cffb916e528f1c74d284d7ad50ab208aa7918
andycr123 <81699558+andycr123@users.noreply.github.com>
2023-10-03 19:40:43 -0500

    changes

---
+++ javascript/app.js
@@ -0,0 +1,81 @@
+// Productos 
+    const productos = [
+        {
+            id: "abrigo-01",
+            titulo: "Abrigo-01",
+            imagen: "",
+            categoria: {
+                nombre: "Abrigos",
+                id: "abrigos"
+            },
+            precio: 1000
+        }, 
+
+        {
+            id: "abrigo-02",
+            titulo: "Abrigo-02",
+            imagen: "",
+            categoria: {
+                nombre: "Abrigos",
+                id: "abrigos"
+            },
+            precio: 1000
+        }, 
+
+        {
+            id: "abrigo-03",
+            titulo: "Abrigo-03",
+            imagen: "",
+            categoria: {
+                nombre: "Abrigos",
+                id: "abrigos"
+            },
+            precio: 1000
+        },
+
+        {
+            id: "abrigo-04",
+            titulo: "Abrigo-04",
+            imagen: "",
+            categoria: {
+                nombre: "Abrigos",
+                id: "abrigos"
+            },
+            precio: 1000
+        },
+
+        {
+            id: "abrigo-05",
+            titulo: "Abrigo-05",
+            imagen: "",
+            categoria: {
+                nombre: "Abrigos",
+                id: "abrigos"
+            },
+            precio: 1000
+        },
+
+        //Camisetas
+        {
+            id:"camisetas-01",
+            titulo: "Camisetas-01",
+            imagen: "",
+            categoria: {
+                nombre: "Camisetas",
+                id: "camisetas"
+            }, 
+            precio: 1000
+        },
+
+        {
+            id:"camisetas-02",
+            titulo: "Camisetas-02",
+            imagen: "",
+            categoria: {
+                nombre: "Camisetas",
+                id: "camisetas"
+            }, 
+            precio: 1000
+        },
+        
+    ]
